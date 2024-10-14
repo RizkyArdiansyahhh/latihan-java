80e86548690c1c3a8f562e58a17838e8415b6bb9
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-10-14 11:16:12 +0700

    prima

--- latihan/src/Main.java
+++ latihan/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("masukkan bilangan bulat : ");
+        int x = inputUser.nextInt();
+
+        boolean prima = true;
+
+        if(x < 2){
+            prima = false;
+        }else{
+//          ngecek apakah x habis dibagi dengan bilangan
+//            selain dirinya sendiri atau 1
+            for(int i = 2 ; i < x ; i++){
+                if(x % i == 0){
+                    prima = false;
+                    break;
+                }
+            }
+        }
+        if(prima){
+            System.out.println("bilangan prima");
+        }else{
+            System.out.println("bukan bilangan prima");
+        }
+
+
+
+
     }
 }
